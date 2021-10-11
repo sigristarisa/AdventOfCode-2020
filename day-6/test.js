6e64b61574a2f924003d60d64649ab9b76f17a37
sigristarisa <arisasigrist.ch@gmail.com>
2021-10-11 09:50:02 +0200

    initial commit

--- day-6/test.js
+++ day-6/test.js
@@ -1,11 +1,26 @@
 const arr = [
-  ["a", "b", "a", "c"],
-  ["d", "e"],
-  ["f", "f", "a", "g"],
+  ["abc"],
+  ["a", "b", "c"],
+  ["ab", "ac"],
+  ["a", "a", "a", "a"],
+  ["b"],
 ];
 
-const arrNumber = arr.reduce((value, nextValue) => {
-  return value.length + nextValue.length;
+const splitArr = arr.map((elements) => {
+  return elements.map((element) => {
+    return element.split("");
+  });
 });
 
-console.log(arrNumber);
+const overlapLetters = (input) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      let lettersArr = [];
+      for (let k = 0; k < input[i][j].length; k++) {
+        if (!lettersArr.includes(input[i][j][k])) {
+          letterArr.push(input[i][j][k]);
+        }
+      }
+    }
+  }
+};
